919942c6fd8dd7a24b0d945654899c0d7ab0b451
Joaquin Bermudez <jeberme@hotmail.com>
2023-07-03 12:21:15 -0500

    Add project

---
+++ CopyPasteFiles/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+string sourcePath = @"E:\Nueva carpeta\2\2";
+string targetPath = @"C:\Bodas_De_Oro";
+
+DirectoryInfo directoryInfo = new DirectoryInfo(sourcePath);
+var files = directoryInfo.GetFiles().ToList();
+foreach (var file in files)
+{
+    try
+    {
+        Console.WriteLine($"Process file: {file.FullName}");
+        System.IO.File.Move(file.FullName, $"{targetPath}\\{file.Name}");
+        Console.WriteLine("FileProcessed.");
+        //return;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+}
+var arr = "x,y".Split(',');
+var vatiable = String.Format("Value1={0},value2={1}", arr);
+Console.WriteLine(vatiable);    
